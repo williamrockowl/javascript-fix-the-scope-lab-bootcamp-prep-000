6cd230a2aa38d5cd73788b8c91b5364464f0de76
William Saulon <williamsaulon24@gmail.com>
2020-04-11 19:51:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,16 +11,9 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
- function yourAnimal() {
-  // The tests expect this function to return `animal` just like the previous function
-  // However, you cannot simply modify the existing variable declared on line 1 in the global scope 
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: Hard-coding 'cat' below will not work
- var j = 'cat'
- j = animal
-  return animal
-}
+var j = 'cat'
+j = animal
+return animal
 }
 
 function add2(n) {
