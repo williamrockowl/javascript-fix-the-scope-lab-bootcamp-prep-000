e3596cf69035b95efa6e813451f7bce9f8dfc10f
William Saulon <williamsaulon24@gmail.com>
2020-04-11 19:50:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
-  var d = myAnimal
-  d = animal
+ var animal = 'cat';
   return animal
 }
 }
